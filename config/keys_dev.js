25cdbe5c4d81fd666ca5c02d5298392acc937b52
Mahmoud A. Hassan <gfxmoda@gmail.com>
2022-05-08 00:08:31 +0200

    Delete keys_dev.js

--- config/keys_dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-     mongoURI:
-       "mongodb+srv://gfxmoda:014255366qQ$@cluster0.ffjd2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-     secretOrKey: "secret",
-   };
-   
